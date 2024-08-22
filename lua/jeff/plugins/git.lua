ff89ab4c48dfe611a26a90475231e8555d686867
Jeff Fasulkey <fazulk@gmail.com>
2024-08-22 13:53:25 -0700

    checkout

--- lua/jeff/plugins/git.lua
+++ lua/jeff/plugins/git.lua
@@ -15,5 +15,6 @@ return {
     local keymap = vim.keymap
     keymap.set('n', '<leader>gs', neogit.open, { silent = true, noremap = true })
     keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true })
+    keymap.set('n', '<leader>gc', ':Telescope git_branches<CR>', { silent = true, noremap = true })
   end,
 }
