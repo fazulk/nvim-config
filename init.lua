e604512d9cbfedd8d3bbe65026546b52493e0a06
Jeff Fasulkey <fazulk@gmail.com>
2024-08-23 10:06:10 -0700

    update go to defs

--- init.lua
+++ init.lua
@@ -5,10 +5,4 @@ if vim.g.vscode then
 else
   require 'jeff.core'
   require 'jeff.lazy'
-
-  function ReloadConfig()
-    vim.notify('All plugins and configuration reloaded!', vim.log.levels.INFO)
-  end
-
-  vim.keymap.set('n', '<leader>rr', ReloadConfig, { noremap = true, silent = true, desc = 'Reload config and plugins' })
 end
--- lua/jeff/plugins/lsp/lspconfig.lua
+++ lua/jeff/plugins/lsp/lspconfig.lua
@@ -54,13 +54,13 @@ return {
         keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
 
         opts.desc = 'Go to previous diagnostic'
-        keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+        keymap.set('n', '<f20>', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
         opts.desc = 'Go to next diagnostic'
-        keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+        keymap.set('n', '<f8>', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
         opts.desc = 'Show documentation for what is under cursor'
-        keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+        keymap.set('n', 'gh', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
         opts.desc = 'Restart LSP'
         keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts) -- mapping to restart lsp if necessary
